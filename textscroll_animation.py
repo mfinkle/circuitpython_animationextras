7d2f5ca2f7746ca51f27597df47274874fab84d4
Mark Finkle <mark.finkle@gmail.com>
2025-01-12 15:13:33 -0500

    Add a TimeScroll animation based on TextScroll

--- textscroll_animation.py
+++ textscroll_animation.py
@@ -1,3 +1,6 @@
+import re
+import rtc
+
 from adafruit_led_animation.animation import Animation
 import adafruit_framebuf
 
@@ -38,3 +41,65 @@ class TextScroll(Animation):
 
     def reset(self):
         self._frame = 0
+
+
+DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+DAYCOUNT = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334]
+# leap_year = (tm_year % 4 == 0 && tm_year % 100 != 0) || tm_year % 400 == 0
+# tm_yday = DAYCOUNT[tm_mon - 1] + tm_mday + ((tm_mon > 2 && leap_year) ? 1 : 0)
+
+class TimeScroll(TextScroll):
+
+    def __init__(self, grid_object, speed, format, color, font_name='font5x8.bin', name=None):
+        self._format = format
+        self._update = True
+        super().__init__(grid_object, speed, self._strftime(), color, font_name=font_name, name=name)
+
+    def _strftime(self):
+        r = rtc.RTC()
+        dt = r.datetime
+
+        def _replace_func(match_obj):
+            try:
+                match = match_obj.group(0)
+            except AttributeError:
+                return ''
+            else:
+                if match == '%a': return DAYS[dt.tm_wday][:3]
+                if match == '%A': return DAYS[dt.tm_wday]
+                if match == '%b': return MONTHS[dt.tm_mon - 1][:3]
+                if match == '%B': return MONTHS[dt.tm_mon - 1]
+                if match == '%c': return '{:02}/{:02}/{} {:02}:{:02}:{:02}'.format(dt.tm_mon, dt.tm_mday, dt.tm_year, dt.tm_hour, dt.tm_min, dt.tm_sec)
+                if match == '%d': return '{:02}'.format(dt.tm_mday)
+                # if match == '%e': return '{}'.format(dt.tm_mday)
+                if match == '%H': return '{:02}'.format(dt.tm_hour)
+                if match == '%I': return '{:02}'.format((dt.tm_hour + 11) % 12 + 1)
+                if match == '%j': return '{:03}'.format(dt.tm_yday)
+                # if match == '%k': return '{}'.format(dt.tm_hour)
+                # if match == '%l': return '{}'.format((dt.tm_hour + 11) % 12 + 1)
+                if match == '%m': return '{:02}'.format(dt.tm_mon)
+                if match == '%M': return '{:02}'.format(dt.tm_min)
+                if match == '%n': return '{}'.format(dt.tm_mon)
+                if match == '%p': return 'AM' if dt.tm_hour < 12 else 'PM'
+                # if match == '%P': return 'am' if dt.tm_hour < 12 else 'pm'
+                if match == '%S': return '{:02}'.format(dt.tm_sec)
+                if match == '%u': return '{}'.format(dt.tm_wday + 1)
+                if match == '%y': return '{:02}'.format(dt.tm_year)
+                if match == '%Y': return '{}'.format(dt.tm_year)
+
+        return re.sub(r'%[a-zA-Z]', _replace_func, self._format)
+
+    def draw(self):
+        if self._update:
+            self._text = self._strftime()
+            self._update = False
+
+        super().draw()
+
+        if self.cycle_complete:
+            self._update = True
+
+    def reset(self):
+        super().reset()
+        self._update = True
