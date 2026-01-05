3b847a167d076800dac2d07c1c5d57145d1a3b90
Mark Finkle <mark.finkle@gmail.com>
2026-01-05 00:08:02 -0500

    Add support for RGB bitmaps

--- sprite_animation.py
+++ sprite_animation.py
@@ -1,8 +1,36 @@
 from micropython import const
+from displayio import Bitmap, Palette, ColorConverter
 from adafruit_led_animation.animation import Animation
 
 HORIZONTAL = const(1)
 VERTICAL = const(2)
+
+def rgb565_to_rgb888(rgb565):
+    """
+    Convert from an integer representing rgb565 color into an integer
+    representing rgb888 color.
+    :param rgb565: Color to convert
+    :return int: rgb888 color value
+    """
+    # Shift the red value to the right by 11 bits.
+    red5 = rgb565 >> 11
+    # Shift the green value to the right by 5 bits and extract the lower 6 bits.
+    green6 = (rgb565 >> 5) & 0b111111
+    # Extract the lower 5 bits for blue.
+    blue5 = rgb565 & 0b11111
+
+    # Convert 5-bit red to 8-bit red.
+    red8 = round(red5 / 31 * 255)
+    # Convert 6-bit green to 8-bit green.
+    green8 = round(green6 / 63 * 255)
+    # Convert 5-bit blue to 8-bit blue.
+    blue8 = round(blue5 / 31 * 255)
+
+    # Combine the RGB888 values into a single integer
+    rgb888_value = (red8 << 16) | (green8 << 8) | blue8
+
+    return rgb888_value
+
 
 class Sprite(Animation):
 
@@ -14,12 +42,12 @@ class Sprite(Animation):
         super().__init__(grid_object, speed, color=None, name=name)
 
         if orientation == VERTICAL:
-            self._frame_count = (self._bitmap.height // self.pixel_object.height)
-            self._offset_mapper = lambda f: f * self.pixel_object.height
+            self._frame_count = (self._bitmap.height // self._bitmap.width)
+            self._offset_mapper = lambda f: f * self._bitmap.width
             self._pixel_mapper = lambda x, y, o: self._bitmap[x, y + o]
         else:
-            self._frame_count = (self._bitmap.width // self.pixel_object.width)
-            self._offset_mapper = lambda f: f * self.pixel_object.width
+            self._frame_count = (self._bitmap.width // self._bitmap.height)
+            self._offset_mapper = lambda f: f * self._bitmap.height
             self._pixel_mapper = lambda x, y, o: self._bitmap[x + o, y]
 
     on_cycle_complete_supported = True
@@ -28,7 +56,15 @@ class Sprite(Animation):
         frame_offset = self._offset_mapper(self._frame)
         for x in range(0, self.pixel_object.width):
             for y in range(0, self.pixel_object.height):
-                self.pixel_object[x, y] = self._palette[self._pixel_mapper(x, y, frame_offset)]
+                pixel = self._pixel_mapper(x, y, frame_offset)
+                if isinstance(self._palette, Palette):
+                    # Indexed bitmap
+                    color = self._palette[pixel]
+                elif isinstance(self._palette, ColorConverter):
+                    # RGB bitmap
+                    color_565 = self._palette.convert(pixel)
+                    color = rgb565_to_rgb888(color_565)
+                self.pixel_object[x, y] = color
 
         self._frame += 1
         if self._frame >= self._frame_count:
